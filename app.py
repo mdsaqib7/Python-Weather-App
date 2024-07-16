edbd71acc5fc4027cebbbe55be808264732af357
Mohammed Saqib Rahman <mdsaqib.rh@gmail.com>
2024-07-16 14:22:49 -0700

    Added UI to the application

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
+from flask import Flask, render_template, request
 import requests
 
+app = Flask(__name__)
+
 api_key = '40d1ad355b8e2bf6a0ea86ab4653911e'
-city_name = input("Enter city: ")
 
-weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_key}"
-)
 
-if weather_data.json()['cod'] == '404':
-    print("No City Found")
-else:
-    weather = weather_data.json()['weather'][0]['main']
-    temperature = round(weather_data.json()['main']['temp'])
-    print(f"The weather in {city_name} is: {weather}")
-    print(f"The temperature in {city_name} is: {temperature} F degrees")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather = None
+    temperature = None
+    city_name = None
+    if request.method == 'POST':
+        city_name = request.form['city']
+        weather_data = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_key}"
+        )
+        if weather_data.json()['cod'] == '404':
+            weather = "No City Found"
+        else:
+            weather = weather_data.json()['weather'][0]['main']
+            temperature = round(weather_data.json()['main']['temp'])
+    return render_template('index.html', weather=weather, temperature=temperature, city_name=city_name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
